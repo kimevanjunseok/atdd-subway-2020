f7dc1971801c91179595f364d719ef77a2e298ad
kimjunseok <jnunseok@gmail.com>
2020-08-06 16:55:11 +0900

    feat: path 인수 테스트 추가 운임 추가

--- src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
@@ -24,6 +24,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+    private Long 잠실역;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -41,6 +42,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         강남역 = 지하철역_등록되어_있음("강남역");
         양재역 = 지하철역_등록되어_있음("양재역");
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역");
+        잠실역 = 지하철역_등록되어_있음("잠실역");
 
         이호선 = 지하철_노선_등록되어_있음("2호선", "GREEN");
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "RED");
@@ -55,6 +57,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철역_등록되어_있음(삼호선, null, 교대역, 0, 0);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 1, 2);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 남부터미널역, 양재역, 2, 2);
+        지하철_노선에_지하철역_등록되어_있음(삼호선, 양재역, 잠실역, 8, 2);
     }
 
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
@@ -78,14 +81,24 @@ public class PathAcceptanceTest extends AcceptanceTest {
         총_거리와_소요_시간을_함께_응답함(response, 4, 3, 1250);
     }
 
-    @DisplayName("두 역의 운임비용을 조회한다.")
+    @DisplayName("두 역의 운임비용을 조회한다.(추가요금 X)")
     @Test
-    void findFare() {
+    void findFareNotOverCharge() {
         //when
         ExtractableResponse<Response> response = 거리_경로_조회_요청("DURATION", 1L, 3L);
         //then
         적절한_경로를_응답(response, Lists.newArrayList(교대역, 강남역, 양재역));
         총_거리와_소요_시간을_함께_응답함(response, 4, 3, 1250);
+    }
+
+    @DisplayName("두 역의 운임비용을 조회한다.(추가요금 O)")
+    @Test
+    void findFareOverCharge() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청("DISTANCE", 1L, 5L);
+        //then
+        적절한_경로를_응답(response, Lists.newArrayList(교대역, 남부터미널역, 양재역, 잠실역));
+        총_거리와_소요_시간을_함께_응답함(response, 11, 6, 1350);
     }
 
     private Long 지하철_노선_등록되어_있음(String name, String color) {
